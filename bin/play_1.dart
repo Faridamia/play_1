acfd8b29ae57c9afadfa106713d1b25b97f90fd4
Your Name <youremail@yourdomain.com>
2023-09-29 00:05:32 +0600

    first commit

---
+++ bin/play_1.dart
@@ -0,0 +1,5 @@
+import 'package:play_1/play_1.dart' as play_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${play_1.calculate()}!');
+}
---
+++ test/play_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:play_1/play_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
